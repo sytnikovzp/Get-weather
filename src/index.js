762b06c8e1f05ddc1b8473ddeb2725740f75a479
alex <sytnikov.zp@gmail.com>
2024-03-18 19:09:50 +0200

    Fix form & css

--- src/index.js
+++ src/index.js
@@ -1,2 +1,7 @@
 'use strict';
 
+const textName = document.getElementById('city-name-txt');
+const textID = document.getElementById('city-id-txt');
+
+const radioId = document.getElementById('city-id-rbtn');
+const radioName = document.getElementById('city-id-rbtn');
